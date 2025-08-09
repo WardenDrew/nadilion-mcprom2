b96c13769de77b323dcf69fd972163d7db1ef73c
Andrew Haskell <andrew.haskell@ironwatch.net>
2025-08-09 00:08:24 +0000

    config changes

--- maintenance.sh
+++ maintenance.sh
@@ -3,6 +3,11 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 
 source ./stop.sh
 
-rm -f ./minecraft/logs/*
+rm -f minecraft/logs/*
+7z a backups/backup-inprogress.7z world
+
+# Rotate these in the future
+rm -f backups/backup-latest.7z
+mv backups/backup-inprogress.7z backups/backup-latest.7z
 
 source ./start.sh
---
+++ update-mods-list.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ls -1 minecraft/mods > mods.txt
